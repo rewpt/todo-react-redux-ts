5b227165437737cd68c9717debff8a89d9f54abc
rewpt <zpermack@gmail.com>
2022-05-09 19:18:59 -0400

    add react, react redux, axios and redux-thunk to json

--- src/index.tsx
+++ src/index.tsx
@@ -6,32 +6,32 @@ interface AppProps {
   color?: string;
 }
 
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
 
-// class App extends React.Component<AppProps> {
-//   // This is the easiest way to define state in a class based
-//   // component because we just override the state property
-//   // There is different syntax for constructor based state init
-//   state = { counter: 0 };
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
+class App extends React.Component<AppProps> {
+  // This is the easiest way to define state in a class based
+  // component because we just override the state property
+  // There is different syntax for constructor based state init
+  state = { counter: 0 };
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
 
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
 
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App color="red" />, document.querySelector("#root"));
