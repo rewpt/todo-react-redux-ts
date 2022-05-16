cfc68a707498974c650e05caff4607ea68ca1510
rewpt <zpermack@gmail.com>
2022-05-16 12:52:17 -0400

    add reducer with enums

---
+++ src/actions/types.ts
@@ -0,0 +1,4 @@
+export enum ActionTypes {
+  //By default this will be assigned a 0 if = 0 is left off
+  fetchTodos,
+}
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
+import { todosReducer } from "./todos";
 
 export const reducers = combineReducers({
   counter: () => 1,
+  todos: todosReducer,
 });
