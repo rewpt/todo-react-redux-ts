c8acb8c3f9eaa7354c642bc393db7bfffbd622aa
rewpt <zpermack@gmail.com>
2022-05-16 16:40:59 -0400

    remove comments

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,7 +1,6 @@
 import { FetchTodosAction, DeleteTodoAction } from "./todos";
 
 export enum ActionTypes {
-  //By default this will be assigned a 0 if = 0 is left off
   fetchTodos,
   deleteTodo,
 }
