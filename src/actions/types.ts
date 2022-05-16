6ebb23c70f40ce7ea47f8109fd7138c1881ea613
rewpt <zpermack@gmail.com>
2022-05-16 15:10:55 -0400

    Make single Action type export

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,4 +1,9 @@
+import { FetchTodosAction, DeleteTodoAction } from "./todos";
+
 export enum ActionTypes {
   //By default this will be assigned a 0 if = 0 is left off
   fetchTodos,
+  deleteTodo,
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
